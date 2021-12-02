090cd30cbd22a48595ec307bb8138d16ac1604f1
Robert McMullen <robert7mcmullen@hotmail.com>
2021-12-02 14:29:12 +0000

    Day 1 and Day 2 for AOC 2021

---
+++ AOC_2021/AOC_Day1/AOC_Day1.py
@@ -0,0 +1,24 @@
+# Read in input text file
+file = open("AOC_Day1_Input.txt","r")
+# Create an array and populate it with the numbers from the text file
+input = []
+for num in file.readlines():
+    input.append(int(num))
+# Close the input text file
+file.close()
+
+# Create a count and iterate through the array incrementing the count 
+# when the current number is larger than the previous one
+count = 0
+# Part 1 - Answer: 1624
+""" for i in range(1, len(input)):
+    if input[i] > input[i-1]:
+        count += 1 """
+
+#Part 2 - Answer: 1653
+for i in range(3, len(input)):
+    if (input[i]+input[i-1]+input[i-2]) > (input[i-1]+input[i-2]+input[i-3]):
+        count += 1
+
+# Print the count
+print(count)
---
+++ AOC_2021/AOC_Day2/AOC_Day2.py
@@ -0,0 +1,50 @@
+# Read in input text file
+file = open("AOC_Day2_Input.txt","r")
+# Create an array and populate it with the commands and numbers as a pair 
+# from each line from the text file
+input = []
+for line in file.readlines():
+    input.append(line.split())
+file.close()
+
+# Part 1 - Answer: 1693300
+# Create a horizontal count and a depth count, iterate through the array 
+# incrementing the horizontal count by the current number when the current command 
+# is forward, decrementing the depth count by the current number when the current 
+# command is up and incrementing the depth count by the current number when the
+# current command is down
+
+""" horizontal = 0
+depth = 0
+
+for i in range(0, len(input)):
+    if input[i][0] == "forward":
+        horizontal += int(input[i][1])
+    elif input[i][0] == "up":
+        depth -= int(input[i][1])
+    else:
+        depth += int(input[i][1]) """
+
+# Part 2 - Answer: 1857958050
+# Create a horizontal count, a depth count and an aim count, iterate through the array 
+# incrementing the horizontal count by the current number and the depth count by the 
+# current number multiplied by the current aim count when the current command 
+# is forward, decrementing the aim count by the current number when the current 
+# command is up and incrementing the aim count by the current number when the
+# current command is down
+
+horizontal = 0
+depth = 0
+aim = 0
+
+for i in range(0, len(input)):
+    if input[i][0] == "forward":
+        horizontal += int(input[i][1])
+        depth += (int(input[i][1]) * aim)
+    elif input[i][0] == "up":
+        aim -= int(input[i][1])
+    else:
+        aim += int(input[i][1])
+
+# Print the answer
+print(horizontal * depth)
